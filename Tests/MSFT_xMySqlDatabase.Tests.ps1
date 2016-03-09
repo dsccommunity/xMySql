70d9635acc7a65450bb3cbd9986b851455698ebe
QIU Quan <jackqq@gmail.com>
2016-03-09 22:11:03 +0800

    Add import for local MSFT_xMySqlUtilities in Tests.
    In order to facilitate unit testing in development environments where
    this module is not installed, and so as to ensure the consistency of
    code under test by eliminating the dependency on the installed version
    of this module.

--- Tests/MSFT_xMySqlDatabase.Tests.ps1
+++ Tests/MSFT_xMySqlDatabase.Tests.ps1
@@ -29,6 +29,12 @@ if (Get-Module xMySql)
     Remove-Module xMySql
 }
 
+if (Get-Module MSFT_xMySqlUtilities)
+{
+    Remove-Module MSFT_xMySqlUtilities
+}
+
+Import-Module (Join-Path $here -ChildPath "..\MSFT_xMySqlUtilities.psm1")
 Import-Module (Join-Path $here -ChildPath "..\DSCResources\MSFT_xMySqlDatabase\MSFT_xMySqlDatabase.psm1")
 Import-Module (Join-Path $here -ChildPath "..\xMySql.psd1")
 
