ce6d27df56550eb951aae5f3f78bd6ab377dc159
Indhu Sivaramakrishnan <indhukrishna@live.com>
2016-08-11 15:56:19 -0700

    Added param block to Pester test to suppress PSSA rule since suppressing in the scope of InModuleScope does not work

--- Tests/MSFT_xMySqlUser.Tests.ps1
+++ Tests/MSFT_xMySqlUser.Tests.ps1
@@ -1,3 +1,6 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidUsingConvertToSecureStringWithPlainText', '')]
+param
+()
 $here = Split-Path -Parent $MyInvocation.MyCommand.Path
 
 if (! (Get-Module xDSCResourceDesigner))
